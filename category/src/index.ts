aca93721496ebf77bccfbdbda6ddfbc7a7276bd8
RivkyShachar <rivkivider@gmail.com>
2024-03-03 13:03:11 +0200

    vlkv

--- category/src/index.ts
+++ category/src/index.ts
@@ -21,28 +21,28 @@ const start = async () => {
   // if (!process.env.POSTGRES_DB) {
   //   throw new Error("POSTGRES_DB must be defined");
   // }
-  if (!process.env.NATS_CLIENT_ID) {
-    throw new Error("NATS_CLIENT_ID must be defined");
-  }
-  if (!process.env.NATS_URL) {
-    throw new Error("NATS_URL must be defined");
-  }
-  if (!process.env.NATS_CLUSTER_ID) {
-    throw new Error("NATS_CLUSTER_ID must be defined");
-  }
+  // if (!process.env.NATS_CLIENT_ID) {
+  //   throw new Error("NATS_CLIENT_ID must be defined");
+  // }
+  // if (!process.env.NATS_URL) {
+  //   throw new Error("NATS_URL must be defined");
+  // }
+  // if (!process.env.NATS_CLUSTER_ID) {
+  //   throw new Error("NATS_CLUSTER_ID must be defined");
+  // }
 
-  try {
-    await natsWrapper.connect(
-      process.env.NATS_CLUSTER_ID,
-      process.env.NATS_CLIENT_ID,
-      process.env.NATS_URL
-    );
-    natsWrapper.client.on("close", () => {
-      console.log("NATS connection closed!");
-      process.exit();
-    });
-    process.on("SIGINT", () => natsWrapper.client.close());
-    process.on("SIGTERM", () => natsWrapper.client.close());
+  // try {
+  //   await natsWrapper.connect(
+  //     process.env.NATS_CLUSTER_ID,
+  //     process.env.NATS_CLIENT_ID,
+  //     process.env.NATS_URL
+  //   );
+  //   natsWrapper.client.on("close", () => {
+  //     console.log("NATS connection closed!");
+  //     process.exit();
+  //   });
+  //   process.on("SIGINT", () => natsWrapper.client.close());
+  //   process.on("SIGTERM", () => natsWrapper.client.close());
 
     // new ExampleListener(natsWrapper.client).listen();
 
@@ -56,9 +56,9 @@ const start = async () => {
   
     //   await pool.connect();
     //   console.log('Connected to PostgreSQL');
-  } catch (err) {
-    console.error(err);
-  }
+  // } catch (err) {
+  //   console.error(err);
+  // }
 
   app.listen(3000, () => {
     console.log("listening to auth at port 3000!");
